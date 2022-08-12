8fd56b5e99cba39e716027370b99015580468cd7
bc-johnhebron <john.hebron@bigcommerce.com>
2022-08-12 01:45:17 -0500

    Initial commit
    Created using  command.

---
+++ lib/bigcommerce/v3.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "v3/version"
+
+module Bigcommerce
+  module V3
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/bigcommerce/v3_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bigcommerce::V3 do
+  it "has a version number" do
+    expect(Bigcommerce::V3::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
