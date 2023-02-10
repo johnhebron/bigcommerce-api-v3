bd69c95231b61df41056734b555926fe4ed4f1ff
John Hebron <johnhebron@gmail.com>
2023-02-10 03:10:53 -0600

    extract shared_examples

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -4,20 +4,4 @@
 # Resource Shared Examples
 ###########################
 
-RSpec.shared_examples 'an instantiable Resource' do
-  it 'is of the correct type' do
-    expect(resource).to be_a(class_name)
-  end
-
-  it 'contains a Bigcommerce::V3::Client' do
-    expect(resource.client).to be_a(Bigcommerce::V3::Client)
-  end
-
-  it 'has a RESOURCE_URL' do
-    expect(class_name::RESOURCE_URL).to eq(resource_url)
-  end
-end
-
-RSpec.shared_examples 'a bulk .list endpoint' do
-  # @TODO
-end
+require './spec/support/shared_examples/resources/base'
---
+++ spec/support/shared_examples/resources/base.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'an instantiable Resource' do
+  it 'is of the correct type' do
+    expect(resource).to be_a(class_name)
+  end
+
+  it 'contains a Bigcommerce::V3::Client' do
+    expect(resource.client).to be_a(Bigcommerce::V3::Client)
+  end
+
+  it 'has a RESOURCE_URL' do
+    expect(class_name::RESOURCE_URL).to eq(resource_url)
+  end
+end
