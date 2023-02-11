d921ffcd1d625c82f89d88a3d443fecf97672bab
John Hebron <johnhebron@gmail.com>
2023-02-11 01:05:04 -0600

    extracted .bulk_delete from customers spec

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -8,6 +8,7 @@ require './spec/support/shared_examples/resources/base'
 
 # Bulk Resources
 require './spec/support/shared_examples/resources/bulk/bulk_create'
+require './spec/support/shared_examples/resources/bulk/bulk_delete'
 require './spec/support/shared_examples/resources/bulk/bulk_update'
 require './spec/support/shared_examples/resources/bulk/create'
 require './spec/support/shared_examples/resources/bulk/list'
---
+++ spec/support/shared_examples/resources/bulk/bulk_delete.rb
@@ -0,0 +1,109 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'a bulk .bulk_delete endpoint' do
+  let(:resource_action) { 'bulk_delete' }
+  let(:stubs) { stub_request(path: url, response: stubbed_response, verb: :delete) }
+  let(:response) { resource.bulk_delete(ids: params) }
+  let(:fixture) { '' } # successful response body is empty for DELETE request
+
+  context 'when passing a valid customer_ids Array' do
+    context 'when the customer records do exist' do
+      let(:status) { 204 }
+
+      context 'when deleting only one customer' do
+        let(:params) { [42] }
+
+        it 'returns a Bigcommerce::V3::Response' do
+          expect(response).to be_a(Bigcommerce::V3::Response)
+        end
+
+        it 'is a success' do
+          expect(response).to be_success
+        end
+
+        it 'has a .total of nil records' do
+          # because the .total is pulled from the meta hash
+          # which is not returned on a DELETE request
+          expect(response.total).to be_nil
+        end
+
+        it 'has a nil .data' do
+          # since a DELETE request only returns a 204 with no body
+          # the .success? method is the best way to check success
+          expect(response.data).to be_nil
+        end
+      end
+
+      context 'when deleting more than one record' do
+        let(:fixture) { '' }
+        let(:params) { [147, 145] }
+
+        it 'returns a Bigcommerce::V3::Response' do
+          expect(response).to be_a(Bigcommerce::V3::Response)
+        end
+
+        it 'is a success' do
+          expect(response).to be_success
+        end
+
+        it 'has a .total of nil records' do
+          # because the .total is pulled from the meta hash
+          # which is not returned on a DELETE request
+          expect(response.total).to be_nil
+        end
+
+        it 'has a nil .data' do
+          # since a DELETE request only returns a 204 with no body
+          # the .success? method is the best way to check success
+          expect(response.data).to be_nil
+        end
+      end
+    end
+
+    context 'when the records do not exist' do
+      # For the BigCommerce API, a DELETE request for an invalid ID still
+      # returns a 204 success with no body
+      let(:fixture) { '' }
+      let(:status) { 204 }
+      let(:params) { [0] }
+
+      it 'returns a Bigcommerce::V3::Response' do
+        expect(response).to be_a(Bigcommerce::V3::Response)
+      end
+
+      it 'is a success' do
+        expect(response).to be_success
+      end
+
+      it 'has a .total of nil records' do
+        # because the .total is pulled from the meta hash
+        # which is not returned on a DELETE request
+        expect(response.total).to be_nil
+      end
+
+      it 'has a nil .data' do
+        # since a DELETE request only returns a 204 with no body
+        # the .success? method is the best way to check success
+        expect(response.data).to be_nil
+      end
+    end
+  end
+
+  context 'when passing an invalid params Array' do
+    let(:fixture) { '' }
+    let(:params) { %w[string string] }
+
+    it 'raises an error' do
+      expect { response }.to raise_error(Bigcommerce::V3::Error::InvalidArguments)
+    end
+  end
+
+  context 'when passing invalid params' do
+    let(:fixture) { '' }
+    let(:params) { '' }
+
+    it 'raises an error' do
+      expect { response }.to raise_error(Bigcommerce::V3::Error::InvalidArguments)
+    end
+  end
+end
