d0645891c2ef63bf4147feb345815200a8f5c4e4
John Hebron <johnhebron@gmail.com>
2023-02-11 00:59:51 -0600

    Extracted .bulk_update from customers spec

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -8,6 +8,7 @@ require './spec/support/shared_examples/resources/base'
 
 # Bulk Resources
 require './spec/support/shared_examples/resources/bulk/bulk_create'
+require './spec/support/shared_examples/resources/bulk/bulk_update'
 require './spec/support/shared_examples/resources/bulk/create'
 require './spec/support/shared_examples/resources/bulk/list'
 require './spec/support/shared_examples/resources/bulk/retrieve'
